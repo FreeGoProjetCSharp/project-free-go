3708ed275eef954630fa6aea87c5e685cce4bd31
sylvain Gandini <sylvain.gandini@cpnv.ch>
2018-12-04 16:10:40 +0100

    Initialisation du code avec xamarin

---
+++ code/FreeGo/FreeGo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FreeGo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {   
+            
+            var image = new Image { Source = "User.png"};
+            InitializeComponent();
+        }
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
