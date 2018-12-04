e2c88274a356eccddcd138db51864216f7d7ca40
[leo] <[leo.zmoos@cpnv.ch]>
2018-12-04 16:52:38 +0100

    Fin du chapitre analyse+debut conception

---
+++ Code/Version Windows/ProjetFreeGoWindows/ProjetFreeGoWindows/connectionDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ProjetFreeGoWindows
+{
+    class connectionDB
+    {
+        //connection to the database
+        private SQLiteConnection m_dbConnection;
+
+        public connectionDB()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=Splendor.sqlite;Version=3;");
+
+
+        }
+    }
+}
--- Code/Version Windows/ProjetFreeGoWindows/ProjetFreeGoWindows/createaccount.cs
+++ Code/Version Windows/ProjetFreeGoWindows/ProjetFreeGoWindows/createaccount.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace ProjetFreeGoWindows
 {
@@ -16,6 +17,24 @@ namespace ProjetFreeGoWindows
         public frm_createaccount()
         {
             InitializeComponent();
+        }
+
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
         }
 
         private void llblBackLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -30,16 +49,30 @@ namespace ProjetFreeGoWindows
         private void cmdCreateAccount_Click(object sender, EventArgs e)
         {
             string emailaddress = txtMail.Text;
-            
+
+            //if (txtPass.Text != "") { string password = txtPass.Text; } else MessageBox.Show("Entrer un mot de passe");
+
+            string regex = "^(?=.*[a - z])(?=.*[A - Z])(?=.*\\d)(?=.*[^\\da - zA - Z]).{ 8, 15 }$";
+
+            Regex pattern = new Regex(regex);
 
             try
             {
-
-
                 MailAddress mail = new MailAddress(emailaddress);
-                string password = txtPass.Text;
 
-                 
+                if (txtPass.Text != "") pattern.Match(txtPass.Text);
+
+                if (txtPass.Text != "" && txtPassConf.Text != "")
+                {
+                    if (txtPass.Text == txtPassConf.Text)
+                    {
+                        string passwordcrypt = CreateMD5(txtPass.Text);
+                    }
+                }
+
+
+
+
             }
             catch (Exception ex)
             {
